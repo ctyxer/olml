c1e6267252daa5f6b33c35b26998a7eb56a9a4ff
Vladimir Senchuk <ctyxer.email@gmail.com>
2025-08-18 23:09:55 +0300

    feat: Add Error
    This enum, called an Error, will contain all possible crash outcomes.
    Later, all error options will appear in it.

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    Message(String),
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Message(msg) => f.write_str(&msg),
+        }
+    }
+}
+
+impl serde::de::Error for Error {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: std::fmt::Display,
+    {
+        Error::Message(msg.to_string())
+    }
+}
+
+impl serde::ser::Error for Error {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: std::fmt::Display,
+    {
+        Error::Message(msg.to_string())
+    }
+}
